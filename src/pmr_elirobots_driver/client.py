dff82d76a366aeb5cee7e9feeddb2257e360a9d1
Felipe Gomes de Melo <felipegmelo@usp.br>
2025-09-17 14:24:20 -0300

    Update client and server w/ pub/sub

--- src/pmr_elirobots_driver/client.py
+++ src/pmr_elirobots_driver/client.py
@@ -1,17 +1,53 @@
+import time
+
 import zmq
+from pmr_elirobots_msgs.cmd import JointCommand, JointCommandMsg
 
 context = zmq.Context()
 
 #  Socket to talk to server
 print("Connecting to hello world server…")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+
+
+def recreate_socket():
+    socket = context.socket(zmq.PUB)
+    # socket.setsockopt(zmq.RCVTIMEO, 1000)  # 100ms timeout for receiving
+    socket.connect("tcp://localhost:5555")
+
+    return socket
+
+
+socket = recreate_socket()
+
+msg1 = JointCommandMsg()
+
+msg1.cmd = JointCommand(joint1=180, joint2=0, joint3=0, joint4=0, joint5=0, joint6=0)
+
+msg2 = JointCommandMsg()
+
+msg2.cmd = JointCommand(joint1=180, joint2=-90, joint3=0, joint4=0, joint5=0, joint6=0)
 
 #  Do 10 requests, waiting each time for a response
-for request in range(10):
+request = 0
+while True:
+    request += 1
     print(f"Sending request {request} …")
-    socket.send(b"Hello")
 
-    #  Get the reply.
-    message = socket.recv()
-    print(f"Received reply {request} [ {message} ]")
+    if request % 2 == 0:
+        socket.send_multipart([b"cmd", msg1.to_json().encode("utf-8")])
+    else:
+        socket.send_multipart([b"cmd", msg2.to_json().encode("utf-8")])
+
+    print("Socket msg sent")
+
+    # #  Get the reply.
+    # message = None
+
+    # try:
+    #     message = socket.recv()
+    # except zmq.error.Again:
+    #     socket = recreate_socket()
+    # else:
+    #     print(f"Received reply {request} [ {message.decode('utf-8')} ]")
+
+    time.sleep(10)
--- src/pmr_elirobots_driver/server.py
+++ src/pmr_elirobots_driver/server.py
@@ -1,54 +1,83 @@
-import zmq
-from zmq.eventloop.zmqstream import ZMQStream
-# from zmq.eventloop.ioloop import IOLoop
-from tornado.ioloop import IOLoop
-
+# from loguru import logger
+# from logging import Logger
+import logging
 import random
 import time
 
+import zmq
+from pmr_elirobots_msgs.cmd import JointCommandMsg
+from pmr_elirobots_sdk import EC
+from rich.console import Console
 from rich.live import Live
 from rich.table import Table
-from rich.console import Console
+from tornado.ioloop import IOLoop, PeriodicCallback
+from zmq.eventloop.zmqstream import ZMQStream
+
+world_state = {"alive_p1": False, "alive_p2": False}
+
+robot1 = EC("192.168.1.201")
+# robot2 = EC("192.168.1.202", auto_connect=True, enable_log=True)
+robot2 = EC("192.168.1.202", auto_connect=False, enable_log=False)
+
+
+# robot2.robot_servo_on()
 
 
 def generate_table(alive_p1: bool, alive_p2: bool) -> Table:
     table = Table()
+
     table.add_column("ID")
+    table.add_column("Connection")
     table.add_column("Status")
 
-    table.add_row("Player 1", "[red]ERROR" if alive_p1 else "[green]LIVE")
-    table.add_row("Player 2", "[red]ERROR" if alive_p2 else "[green]LIVE")
+    table.add_row(
+        "Robot 1",
+        "[green]LIVE" if hasattr(robot1, "sock_cmd") else "[red]ERROR",
+        robot1.state.name,
+    )
+    table.add_row(
+        "Robot 2",
+        "[green]LIVE" if hasattr(robot2, "sock_cmd") else "[red]ERROR",
+        robot2.state.name,
+    )
 
     return table
 
-world_state = {
-    "alive_p1": True,
-    "alive_p2": True
-}
 
-def update_table():
-    return generate_table(world_state["alive_p1"], world_state["alive_p2"])
+def refresh_console(console: Console):
+    def func():
+        console.clear()
+        console.print(generate_table(world_state["alive_p1"], world_state["alive_p2"]))
+
+    return func
+
+
+def receive_callback(raw_msg: list[bytes]):
+    if robot2.state == EC.RobotState.ERROR:
+        robot2.robot_servo_on()
+
+    # topic = raw_msg[0].decode("utf-8")
+    payload = raw_msg[1].decode("utf-8")
+
+    msg = JointCommandMsg.from_json(payload)
+    robot2.move_joint(msg.cmd.as_list, 10, block=False)
+
+    world_state["alive_p2"] = True
+
+
+def health_check():
+    world_state["alive_p1"] = robot1.state  # type: ignore
+    world_state["alive_p2"] = robot2.state  # type: ignore
+
 
 # Create a ZeroMQ context and socket
 context = zmq.Context()
-socket = context.socket(zmq.REP)
+socket = context.socket(zmq.SUB)
 socket.bind("tcp://*:5555")
+socket.setsockopt_string(zmq.SUBSCRIBE, "cmd")  # Subscribe to a topic
 
 console = Console()
 console.clear()
-
-live = Live(update_table(), console=console, auto_refresh=False)
-live.console.clear()
-def receive_callback(msg):
-    live.console.clear()
-
-    update_table()
-
-    # live.console.print("\n")
-    # live.console.print(f"received {msg[0]}")
-
-    live.refresh()
-    socket.send(b"World")
 
 # Create a ZMQStream from the socket
 stream = ZMQStream(socket)
@@ -56,7 +85,8 @@ stream = ZMQStream(socket)
 # Register the callback for incoming messages
 stream.on_recv(receive_callback)
 
-# Start the event loop (essential for callbacks to work)
-console.print("Starting ZMQStream event loop...")
+console_update = PeriodicCallback(refresh_console(console), 500)
+
+console_update.start()
 
 IOLoop.current().start()
