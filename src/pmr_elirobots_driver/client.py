c0ff2f3d49bfab856f4de3dfec05db1ec77bb2c2
Felipe Gomes de Melo <felipegmelo@usp.br>
2025-09-17 15:59:02 -0300

    Add client usage example

--- src/pmr_elirobots_driver/client.py
+++ src/pmr_elirobots_driver/client.py
@@ -1,3 +1,5 @@
+import time
+
 import zmq
 from pmr_elirobots_msgs.cmd import JointCommand, JointCommandMsg
 
@@ -18,3 +20,20 @@ class Client:
         )
 
         self.socket.send_multipart([self.topic, msg.to_json().encode("utf-8")])
+
+
+if __name__ == "__main__":
+    robot = Client()
+
+    count = 0
+    while True:
+        count += 1
+
+        if count % 2:
+            print("Send cmd1")
+            robot.send_command(joint1=180, joint2=0, joint3=0, joint4=0, joint5=0, joint6=0)
+        else:
+            print("Send cmd2")
+            robot.send_command(joint2=-90)
+
+        time.sleep(10)
